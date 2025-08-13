7f4621fdd5674aca6bd7b28763a6d8b7127592b3
Adityasputra <aditsputra.cv@gmail.com>
2025-08-13 14:35:43 +0700

    feat: variable

---
+++ variable.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "Adits";
+$age = 20;
+
+echo "Name :";
+echo $name;
+echo "\n";
+
+echo "Age :";
+echo $age;
+echo "\n";
+
+// Variable Variables
+$name = "adits";
+$$name = "coding";
+
+echo "\$name = ";
+echo $name;
+echo "\n";
+
+echo "\$adits = ";
+echo $adits;
+echo "\n";
