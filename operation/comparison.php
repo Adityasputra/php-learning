f9544b16f629d0b398b02ad15c16e48fb66a8e1e
Adityasputra <aditsputra.cv@gmail.com>
2025-08-13 16:22:19 +0700

    docs: comparison operations

---
+++ operation/comparison.php
@@ -0,0 +1,34 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+// Equal
+var_dump($a == $b);
+
+// Identical
+var_dump($a === $b);
+
+// Not Equal
+var_dump($a != $b);
+
+// Not Equal
+var_dump($a <> $b);
+
+// Not Identical
+var_dump($a !== $b);
+
+// Less Than
+var_dump($a < $b);
+
+// Greater Than
+var_dump($a > $b);
+
+// Less Than or Equal to
+var_dump($a <= $b);
+
+// Greater Than or Equal to
+var_dump($a >= $b);
+
+// Spaceship
+var_dump($a <=> $b);
