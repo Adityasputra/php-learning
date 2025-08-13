693d0b7490a676f47e4688bdc8e44b5e8213f4e1
Adityasputra <aditsputra.cv@gmail.com>
2025-08-13 16:06:24 +0700

    docs: assignment operations

---
+++ operation/assignment.php
@@ -0,0 +1,18 @@
+<?php
+
+$total = 0;
+
+$vegetable = 5000;
+$fruit = 10_000;
+$rice = 100_000;
+
+$total += $vegetable;
+$total += $fruit;
+$total += $rice;
+
+var_dump($total);
+echo "\n";
+
+echo "Total : ";
+echo $total;
+echo "\n";
