f499074827faf577746112fa3d01f0464d352885
Adityasputra <aditsputra.cv@gmail.com>
2025-08-13 14:36:01 +0700

    feat: constant

---
+++ constant.php
@@ -0,0 +1,10 @@
+<?php
+
+// For best practices, use uppercase letters (UPPER_CASE) to declare constants.
+define("AUTHOR", "Aditya Saputra");
+define("VERSION", "1.0.0");
+
+echo AUTHOR;
+echo "\n";
+echo VERSION;
+echo "\n";
