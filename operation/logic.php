b72769b0a62a9325c0dfbe2d940224dda1d2b129
Adityasputra <aditsputra.cv@gmail.com>
2025-08-13 16:26:36 +0700

    docs: logic operations

---
+++ operation/logic.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = false;
+$b = true;
+
+// And
+var_dump($a && $b);
+var_dump($a and $b);
+
+// Or
+var_dump($a || $b);
+var_dump($a or $b);
+
+// Not
+var_dump(!$a);
+
+// Xor
+var_dump($a xor $b);
