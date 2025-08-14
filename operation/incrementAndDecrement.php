f97d3835b8a37d18fcb0d7d224f81040403f1a45
Adityasputra <aditsputra.cv@gmail.com>
2025-08-14 20:51:17 +0700

    docs: increment and decrement

---
+++ operation/incrementAndDecrement.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 0;
+
+// Post Increment
+$a++;
+var_dump($a);
+
+// Post Decrement
+$a--;
+var_dump($a);
+
+// Pre Increment
+++$a;
+var_dump($a);
+
+// Pre Decrement
+--$a;
+var_dump($a);
