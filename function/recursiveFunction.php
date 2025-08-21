22f668709b73dcc478b0861cdd6764cca871be77
Adityasputra <aditsputra.cv@gmail.com>
2025-08-21 18:47:28 +0700

    docs: recursive function

---
+++ function/recursiveFunction.php
@@ -0,0 +1,23 @@
+<?php
+
+function factorialLoop(int $value): int
+{
+    $total = 1;
+    for ($i = 1; $i <= $value; $i++) {
+        $total *= $i;
+    }
+    return $total;
+}
+
+echo factorialLoop(6);
+
+function factorialRecursive(int $value): int
+{
+    if ($value == 1) {
+        return 1;
+    } else {
+        return $value * factorialRecursive($value - 1);
+    }
+}
+
+echo factorialRecursive(6);
