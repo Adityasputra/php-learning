d83ff3866c6a8fbecce23ea960f535ec4caaded1
Adityasputra <aditsputra.cv@gmail.com>
2025-08-21 18:47:17 +0700

    docs: variable function

--- function/variableFunction.php
+++ function/variableFunction.php
@@ -1,2 +1,26 @@
 <?php
 
+function foo()
+{
+    echo "FOO" . PHP_EOL;
+}
+
+
+function bar()
+{
+    echo "BAR" . PHP_EOL;
+}
+
+$functionName = "foo";
+$functionName();
+
+$functionName = "bar";
+$functionName();
+
+function sayHello(string $name, $filter): string
+{
+    $finalName = $filter($name);
+    return $finalName . PHP_EOL;
+}
+
+echo sayHello("aditya saputra", "strtoupper");
