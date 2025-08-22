e03afd985cbeae452c582c756abe7c4f83cb1d58
Adityasputra <aditsputra.cv@gmail.com>
2025-08-22 23:43:29 +0700

    docs: string function

---
+++ function/stringFunction.php
@@ -0,0 +1,9 @@
+<?php
+
+var_dump(join(",", [1, 2, 3, 4, 5]));
+var_dump(implode(" + ", [1, 2, 3, 4, 5]));
+var_dump(explode(" ", "Aditya Saputra"));
+var_dump(strtolower("ADITYA SAPUTRA"));
+var_dump(strtoupper("aditya saputra"));
+var_dump(substr("Aditya Saputra", 1, 2));
+var_dump(trim("   Aditya    "));
