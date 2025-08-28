6e16ef475873c4f9e0a1e39534e2bb4479996cd7
Adityasputra <aditsputra.cv@gmail.com>
2025-08-28 22:03:01 +0700

    docs: variable scope

--- variableScope.php
+++ variableScope.php
@@ -5,10 +5,30 @@ $name = "Adits";
 // Global
 function sayHello()
 {
-    global $name;
+    global $name; // global keyword
     echo "Hello $name" . PHP_EOL;
 }
 
 sayHello();
 
+// $GLOBAL variable
+function sayGoodBye()
+{
+    echo "Goodbye {$GLOBALS["name"]}" . PHP_EOL;
+}
 
+sayGoodBye();
+
+// Static Scope
+
+function increment()
+{
+    static $counter = 1;
+    echo "Counter : $counter" . PHP_EOL;
+
+    $counter++;
+}
+
+increment();
+increment();
+increment();
