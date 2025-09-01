f8d02537fed46bb3c7da918bf5d75830c709888a
Adityasputra <aditsputra.cv@gmail.com>
2025-09-02 02:36:02 +0700

    refactor: update app.php to improve output formatting and include correct files

--- basic/todo/app.php
+++ basic/todo/app.php
@@ -1,16 +1,13 @@
 <?php
 
-include_once __DIR__ . "/views/showTodolist.php";
-$todolist = include_once __DIR__ . "/models/todolist.php";
+include_once __DIR__ . "/views/viewTodolist.php";
+$todolist = include_once __DIR__ . "/models/modelTodolist.php";
 
-
-echo <<<TEXT
-=============
-Welcome To TodoNext
-An application to help you manage your tasks
-=============
-
-
-TEXT;
+$width = 50;
+echo str_repeat("=", $width) . PHP_EOL;
+echo str_pad("Welcome to TodoNext", $width, " ", STR_PAD_BOTH) . PHP_EOL;
+echo str_pad("An application to help you manage your tasks", $width, " ", STR_PAD_BOTH) . PHP_EOL;
+echo str_repeat("=", $width) . PHP_EOL;
+echo PHP_EOL;
 
 showTodolist($todolist);
